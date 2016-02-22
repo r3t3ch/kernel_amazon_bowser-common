0b2a7241d44e130e9df63bbe4344295b6960ef57
transi <trance@hotbox.ru>
2016-02-22 21:33:31 +0200

    cpr_coefs_sys

--- include/video/omapdss.h
+++ include/video/omapdss.h
@@ -463,6 +463,10 @@ struct omap_overlay_manager_info {
 	bool cpr_enable;
 	struct omap_dss_cpr_coefs cpr_coefs;
 	u8 gamma;
+	
+	/* cpr settings provided via the sysfs entry */
+	bool cpr_enable_sys;
+	struct omap_dss_cpr_coefs cpr_coefs_sys;	
 };
 
 struct omap_overlay_manager {
